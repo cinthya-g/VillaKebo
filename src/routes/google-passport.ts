b27206cab3a6b111f51c037eb5e6afb8b5b0c15b
Cinthya <cinthya.lorena.gonzalez@hotmail.com>
2024-04-18 09:33:56 -0600

    Add basic logout verification

--- src/routes/google-passport.ts
+++ src/routes/google-passport.ts
@@ -1,15 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import passport from 'passport';
 import { User } from '../../types/user';
 
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
     res.send(`<h1>Bienvenido, ${req.user ? (req.user as User).displayName : 'Invitado'}!</h1>`);
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', (req: Request, res: Response) => {
     res.send('<a href="/google-passport/google-auth">Iniciar sesion con Google</a>');
 })
 
@@ -21,10 +21,25 @@ router.get('/google/callback',
     passport.authenticate('google', { 
         failureRedirect: '/google-passport/login' 
     }),
-    (req, res) => {
-      res.redirect('/google-passport/'); // Enviar a home
+    (req: Request, res: Response) => {
+      res.redirect('/google-passport/'); 
     }
 );
+
+router.get('/logout', (req: Request, res: Response, next: NextFunction) => {
+    req.logout(function(err) {
+        if (err) { 
+            return next(err); 
+        } 
+        req.session.destroy(function (err) {
+            if (err) {
+                return next(err); 
+            }
+            res.clearCookie('connect.sid'); 
+            res.redirect('/google-passport/login'); 
+        });
+    });
+});
 
 
 export default router;
