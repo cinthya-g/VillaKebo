5290b51f5b659e8b5571acfd58f9ef33eaabe903
Cinthya <cinthya.lorena.gonzalez@hotmail.com>
2024-05-01 22:28:36 -0600

    testing render pt2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,7 +14,7 @@ import caretakerRouter from "./caretaker";
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 /**
